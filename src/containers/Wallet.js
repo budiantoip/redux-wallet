c2cc7d35f5887c0090a79d9f010bcb9b67afe53e
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-07-01 23:07:56 +0700

    Create wallet

---
+++ src/containers/Wallet.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Wallet = () => {
+  return (
+    <div>
+        <h1>My Wallet</h1>
+        <h2>Balance: Rp 50.000</h2>
+        <button>Withdraw Rp 10.000</button>
+        <button>Deposit Rp 10.000</button>
+    </div>
+  )
+}
+
+export default Wallet
