358817e3a9e567168951278c758491da83d10a7c
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-07-04 22:23:15 +0700

    fetch user using middleware

--- src/containers/Wallet.js
+++ src/containers/Wallet.js
@@ -1,17 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { deposit, selectBalance, selectUser, withdraw } from '../reducers/walletSlice';
+import { deposit, selectBalance, selectUser, userAsync, withdraw } from '../reducers/walletSlice';
 
 const Wallet = () => {
     const balance = useSelector(selectBalance);
     const user = useSelector(selectUser);
     const [currentAmount, setCurrentAmount] = useState();
+    const [id, setId] = useState();
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(userAsync(3));
+    }, [])
 
     return (
         <div>
-            <h1>{user} Wallet</h1>
+            <img src={user.avatar} alt='avatar' />
+            <h1>{user.first_name} Wallet</h1>
+            <input
+                type="number"
+                value={id}
+                placeholder="1 to 12"
+                onChange={e => setId(parseInt(e.target.value))}
+            />
+            <button onClick={() => dispatch(userAsync(id))}>Get User</button>
+
             <h2>Balance: Rp {balance.toLocaleString("ID")}</h2>
             <button onClick={() => dispatch(withdraw(10000))}>Withdraw Rp 10.000</button>
             <button onClick={() => dispatch(deposit(10000))}>Deposit Rp 10.000</button>
--- src/reducers/walletSlice.js
+++ src/reducers/walletSlice.js
@@ -1,21 +1,46 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = { user: 'Monica', balance: 100000 }
+const initialState = { user: {}, balance: 100000 }
+
+// Ini adalah Thunk agar kita bisa melakukan async logic
+// Dia dapat di-dispatch seperti action biasa
+export const userAsync = createAsyncThunk(
+    'wallet/fetchUser',
+    async (id) => {
+        const response = await axios.get(`https://reqres.in/api/users/${id}`);
+        // Return menjadi `fulfilled` action payload
+        // Karena axios membungkus dalam data, dan return dari API juga ada data
+        // Makanya datanya jadi 2x
+        return response.data.data;
+    }
+);
 
 const walletSlice = createSlice({
-  name: 'wallet',
-  initialState,
-  reducers: {
-    deposit: (state, action) => {
-      // Di sini seakan-akan langsung mengubah state,
-      // Tapi sebenarnya tidak karena menggunakan Immer library
-      // Untuk nama parameternya akan selalu menjadi payload
-      state.balance += action.payload;
+    name: 'wallet',
+    initialState,
+    reducers: {
+        deposit: (state, action) => {
+            state.balance += action.payload;
+        },
+        withdraw: (state, action) => {
+            state.balance -= action.payload;
+        },
     },
-    withdraw: (state, action) => {
-      state.balance -= action.payload;
+    // `extraReducers` adalah field tambahan untuk menghandle actions
+    // yang dibuat oleh createAsyncThunk
+    extraReducers: (builder) => {
+        builder
+            .addCase(userAsync.fulfilled, (state, action) => {
+                state.user = action.payload;
+            })
+            .addCase(userAsync.pending, () => {
+                console.log(`loading`);
+            })
+            .addCase(userAsync.rejected, () => {
+                console.log(`fail to get user`);
+            });
     },
-  },
 });
 
 // Akan dibuatkan object action otomatis dari sini
