b3d98c27d3c2673d7479ffdbbfd280bfc4ec91a2
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-07-07 18:43:31 +0700

    add middleware for colorAPI

--- src/app/store.js
+++ src/app/store.js
@@ -4,10 +4,13 @@ import walletSlice from '../reducers/walletSlice';
 import { colorAPI } from '../services/colorAPI';
 
 const store = configureStore({
-    reducer: {
-      wallet: walletSlice,
-      [colorAPI.reducerPath]: colorAPI.reducer
-    }
+  reducer: {
+    wallet: walletSlice,
+    [colorAPI.reducerPath]: colorAPI.reducer
+  },
+  // Menambahkan middleware untuk caching, invalidation, polling
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(colorAPI.middleware),
 });
 
 export default store;
